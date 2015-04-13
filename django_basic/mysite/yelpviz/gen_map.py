221f7f4c88962a831cd3f2f1ff8b812d5c10ad72
EC2 Default User <nmiodice@gmail.com>
2015-04-13 23:19:43 +0000

    submission for karthik

--- django_basic/mysite/yelpviz/gen_map.py
+++ django_basic/mysite/yelpviz/gen_map.py
@@ -7,19 +7,22 @@ from pylab import *
 import pylab
 import views
 import cx_Oracle
+import random
+from sklearn.cluster import DBSCAN
+
 
 def generate_map(x, y, z, output):
-	print(x)
-	print(y)
-	print(z)
+	print(x.shape)
+	print(y.shape)
+	print(z.shape)
 
 	# Setting boundaries of images (should be same at maps.html)
-	# x = np.insert(x, 0, -129.3) # At position 0 SW long
-	# x = np.insert(x, 1, -62.3) # At position 1 NE long
-	# y = np.insert(y, 0, 23.7) # At position 0 SW lat
-	# y = np.insert(y, 1, 49.5) # At position 1 NE lat
-	# z = np.insert(z, 0, 0) # 0 stars for bound
-	# z = np.insert(z, 1, 0) # 0 stars for bound
+	x = np.insert(x, 0, -129.3) # At position 0 SW long
+	x = np.insert(x, 1, -62.3) # At position 1 NE long
+	y = np.insert(y, 0, 23.7) # At position 0 SW lat
+	y = np.insert(y, 1, 49.5) # At position 1 NE lat
+	z = np.insert(z, 0, 0) # 0 stars for bound
+	z = np.insert(z, 1, 0) # 0 stars for bound
 
 	# Set up a regular grid of interpolation points
 	xi, yi = np.linspace(x.min(), x.max(), 500), np.linspace(y.min(), y.max(), 500)
@@ -36,39 +39,95 @@ def generate_map(x, y, z, output):
 	# Generate figure from interpolation points
 	plt.figure(figsize=(40,20))
 	plt.imshow(mask, vmin=1, vmax=5, origin='lower',
-	           extent=[x.min(), x.max(), y.min(), y.max()], alpha = 0.4)
+	           extent=[x.min(), x.max(), y.min(), y.max()], alpha = 1)
 
 	# Plot the actual positions
 
-	col_map = matplotlib.colors.ListedColormap(['green', 'red'])
-	plt.scatter(x, y, c=z, cmap=col_map)
+	# col_map = matplotlib.colors.ListedColormap(['green', 'red'])
+	plt.scatter(x, y, c=z)#, cmap=col_map)
 	plt.axis('off')
 	#plt.colorbar()
 	#plt.show()
 	savefig(output, transparent = True, bbox_inches='tight')
 
+"""
+Performs clustering on a matrix, X, using features FEATURES. FEATURES
+determines which columns will be used to cluster, and these columns will be
+treated as numeric values. For example, if FEATURES = [0, 2], then columns
+0 and 2 will be used as features to cluster the data on, but the returned 
+matrix includes all original features. The cluster value will be inserted
+as the first column. Data that was not included in any cluster is given a
+cluster ID of -1
+"""
+def cluster(x, features):
+	x_to_clus = x[:, features].astype(float)
+	# run DBSCAN clustering algorithm for fast clustering
+	db = DBSCAN(eps = .38, min_samples = 2).fit(x_to_clus)
+	labels = db.labels_
+	labels = labels.reshape(len(labels), 1)
+	x_clus = np.append(labels, x, axis = 1)
+	return x_clus
+
+"""
+Removes rows that have no cluster assignment
+"""
+def differentiate_no_cluster(x):
+	new_clus = -1
+	for i in range(len(x)):
+		if x[i, 0] == -1:
+			x[i, 0] = new_clus
+			new_clus -= 1
+	# x = x[x[:, 0].A1 != -1, :]
+	return x
+
 if __name__ == '__main__':
-	sql = "Select b.latitude, b.longitude, br.avg_rating " + \
+	sql = "Select b.longitude, b.latitude, br.avg_rating " + \
 		"From business b, business_rating br " + \
 		"Where b.business_id = br.business_id " + \
-		"And b.name = 'McDonald''s' ORDER BY br.avg_rating DESC"
+		"And b.name = 'McDonald''s' ORDER BY b.longitude DESC, b.latitude DESC"
 
 	con = cx_Oracle.connect(views.rds_conn_str)
 	cur = con.cursor()
 	cur.execute(sql)
 	result = cur.fetchall()
-	data = np.zeros((len(result), 3))
+	# data = np.zeros((len(result), 3))
+	data = [];
 
+	# i = 0
+	for res_row in result:
+		row = []
+		row.append(float(res_row[0]) + random.random())
+		row.append(float(res_row[1]) + random.random())
+		row.append(float(res_row[2]) + random.random())
+
+		if (row[1] <= 49.5 and row[1] >= 23.7 and row[0] <= -62.3 and row[0] >= -129.3):
+			data.append(row)
+		
+
+		# data[i, 0] = float(row[0])
+		# data[i, 1] = float(row[1])
+		# data[i, 2] = float(row[2])
+		# i += 1
+
+	data = np.matrix(data)
+	clusters = cluster(data, [0, 1])
+	clusters = differentiate_no_cluster(clusters)
+
+	clust_id = np.unique(clusters[:, 0].A1)
+	data = np.zeros((len(clust_id), 3))
+	print(data)
 	i = 0
-	for row in result:
-		data[i, 0] = row[0]
-		data[i, 1] = row[1]
-		data[i, 2] = row[2]
+	for c in clust_id:
+		data[i, 0] = np.average(clusters[(clusters[:, 0] == c).A1, 1])
+		data[i, 1] = np.average(clusters[(clusters[:, 0] == c).A1, 2])
+		data[i, 2] = np.average(clusters[(clusters[:, 0] == c).A1, 3])
 		i += 1
+
+
 
 	#x = -126.7+61.8*np.random.rand(30)
 	#y = 16.7*np.random.rand(30) + 30.7
 	#z = [3, 2, 3, 4, 5, 5, 4, 2, 1, 1, 2, 2, 3, 4, 5, 5, 4, 2, 1, 2, 3, 2, 3, 4, 5, 3, 2, 3, 4, 5]
-	generate_map(data[:, 1], data[:, 0], data[:, 2], 'tmp.png')
+	generate_map(data[:, 0], data[:, 1], data[:, 2], 'static/yelpviz/tmp.png')
 	# generate_map(x, y, z, 'tmp.png')
 
--- django_basic/mysite/yelpviz/static/yelpviz/login.js
+++ django_basic/mysite/yelpviz/static/yelpviz/login.js
@@ -1,37 +1,18 @@
 /***************************************************************
- * Generic Login code & user account handling
- */
- var last_login = null;
 /**
  * Handles UI response to login/logout events
  */
 function login_event(is_loggedin, type) {
-
-  if (is_loggedin) {
-    last_login = type;
-  }
-
-  /* probably some async call we dont care about */
-  if (!is_loggedin && last_login == null)
-    return;
-
   if (!is_loggedin) {
-      last_login = null;
       /* shut off profile picture */
       document.getElementById('profile-pic').setAttribute('style', 'display: none');
       document.getElementById('profile-pic').setAttribute('src', '');
-
-      /* all google login stuff can be shown except logout button */
-      document.getElementById('google-login-details').setAttribute('style', 'display: block');
-      document.getElementById('google-login-button').setAttribute('style', 'display: block');
-      document.getElementById('google-logout-button').setAttribute('style', 'display: none');
 
       /* all FB stuff, except profile attributes, can be shown */
       document.getElementById('fb-login-details').setAttribute('style', 'display: block');
       document.getElementById('fb-logged-in-gui').setAttribute('style', 'display: none');
   } else if (type == "facebook") {
       /* shut off google UI, turn on FB profile stuff */
-      document.getElementById('google-login-details').setAttribute('style', 'display: none');
       document.getElementById('fb-logged-in-gui').setAttribute('style', 'display: block');
 
       FB.api("/me/picture?width=50&height=50",  function(response) {
@@ -39,93 +20,9 @@ function login_event(is_loggedin, type) {
         document.getElementById('profile-pic').setAttribute('src', response.data.url);
         console.log('Retrieved Facebook profile picture');
       });  
-  } else if (type == "google") {
-      document.getElementById('google-login-details').setAttribute('style', 'display: block');
-      document.getElementById('google-logout-button').setAttribute('style', 'display: block');
-      document.getElementById('google-login-button').setAttribute('style', 'display: none');
-      document.getElementById('fb-login-details').setAttribute('style', 'display: none');
-      
-
-      /* load user profile & get URL to profile picture */
-      gapi.client.load('plus','v1', function() {
-      var request = gapi.client.plus.people.get({
-          'userId': 'me'
-        });
-        request.execute(function(resp) {
-          if (last_login == 'google') {
-            document.getElementById('profile-pic').setAttribute('style', 'display: block');
-            document.getElementById('profile-pic').setAttribute('src', resp.image.url);
-          }
-          console.log('Retrieved Google+ profile picture');
-        });
-      });
-  }
-}
- 
-/***************************************************************
- *
- * GOOGLE LOGIN BELOW
- */
-var clientId = '9623654849-crp8t34rpcaeukbsbri3spv8v7qvsbjo.apps.googleusercontent.com';
-var apiKey = 'AIzaSyBUo1pyUUbezj2JjvmX21RrCDPIIxW6GaY';
-var scopes = 'https://www.googleapis.com/auth/plus.me';
-function handleClientLoad() {
-  gapi.client.setApiKey(apiKey);
-  window.setTimeout(checkAuth,1);
-}
-
-function checkAuth() {
-  gapi.auth.authorize({
-    client_id: clientId, 
-    scope: scopes, 
-    immediate: true}, handleAuthResult);
-}
-
-function signinCallback(authResult) {
-  console.log(authResult)
-  if (authResult['status']['signed_in']) {
-    // Update the app to reflect a signed in user
-    // Hide the sign-in button now that the user is authorized, for example:
-    login_event(true, "google");
-  } else {
-    // Update the app to reflect a signed out user
-    // Possible error values:
-    //   "user_signed_out" - User is signed-out
-    //   "access_denied" - User denied access to your app
-    //   "immediate_failed" - Could not automatically log in the user
-    console.log('Sign-in state: ' + authResult['error']);
-    login_event(false, "google");
   }
 }
 
-function handleAuthClick(event) {
-  gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
-  return false;
-}
-
-function googleLogin() {
-  var additionalParams = {
-   'callback': signinCallback
-  };
-  gapi.auth.signIn(additionalParams);
-}
-
-function googleLogout() {
-  gapi.auth.signOut();
-}
-
-/**
- * Called when Google+ API loads */
-function render() {
-  var additionalParams = {
-    'theme' : 'dark',
-    'callback' : 'signinCallback',
-  };
-  gapi.signin.render('google-login-button', additionalParams);
-  if (last_login != null) {
-    document.getElementById('google-login-button').setAttribute('style', 'display: none');
-  }
-}
 
 /***************************************************************
  *
@@ -159,9 +56,9 @@ window.fbAsyncInit = function() {
     version    : 'v2.2'
   });
 
-  // FB.getLoginStatus(function(response) {
-  //   statusChangeCallback(response);
-  // });
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
 };
 
 // Load the SDK asynchronously
